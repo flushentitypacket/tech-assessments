3fb09b120c3b63a4764729328aab089599ba39bd
Ronald Hong <ronniejhong@gmail.com>
2019-04-18 15:48:03 -0400

    Fix DNS routing issues.

--- fullstack/api/deploy/route53.tf
+++ fullstack/api/deploy/route53.tf
@@ -2,6 +2,20 @@ resource "aws_route53_zone" "primary_route" {
   name = "${var.domain}"
 }
 
+data "aws_route53_zone" "luminopia_dev" {
+  name = "luminopia-dev.com."
+}
+
+resource "aws_route53_record" "tech_assessment_ns" {
+  zone_id = "${data.aws_route53_zone.luminopia_dev.zone_id}"
+  type    = "NS"
+  ttl     = "60"
+
+  # TODO: This isn't very modular. Should be derived from var.domain or something like that.
+  name    = "tech-assessment-fullstack"
+  records = ["${aws_route53_zone.primary_route.name_servers}"]
+}
+
 resource "aws_route53_record" "main" {
   zone_id = "${aws_route53_zone.primary_route.id}"
   name    = "${var.domain}"
